d546abc1952e3261e919cde1c92627f8e33cbc58
Jericho V <72720530+JerichoValerio@users.noreply.github.com>
2022-12-08 11:20:32 -0800

    reformatted code

--- Exams/Final-practice/src/numberlinetravel/NumberLineTravel.java
+++ Exams/Final-practice/src/numberlinetravel/NumberLineTravel.java
@@ -4,49 +4,48 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.event.SwingPropertyChangeSupport;
+
 public class NumberLineTravel {
 
-	/**
-	 * Determine how many unique cities you can visit via monorail if you are
-	 * limited to k kilometers per trip. You are permitted an infinite number of
-	 * trips. You are given an array that represents city locations and you
-	 * start your journey at the first city in the array.
-	 * 
-	 * @param x
-	 *          is an array that represents the cities on a number line. The
-	 *          distance between city x[i] and city x[j] is | x[i]-x[j] |
-	 *          kilometers.
-	 * @param k
-	 *          represents the maximum distance, in kilometers, that one can
-	 *          travel in one monorail trip. k should be greater than 0.
-	 * @return the number of new cities that one can visit starting from x[0].
-	 *         x[0] is not included in the count.
-	 */
-	public static int howManyCitiesCanIVisit(int[] x, int k) {
-		// todo: Implement this method
-		Set<Integer> visitedSet = new HashSet<Integer>();
-		visitedSet.add(x[0]);
-		int currentCity = x[0];
-		for (int i = 1; i < x.length; i++) {
-			if (valid(x[i], k, visitedSet) && !visitedSet.contains(x[i])) {
-				currentCity = x[i];
-				visitedSet.add(x[i]);
-				i = 0;
-			}
-		}
-		return visitedSet.size() - 1;
-	}
+    /**
+     * Determine how many unique cities you can visit via monorail if you are
+     * limited to k kilometers per trip. You are permitted an infinite number of
+     * trips. You are given an array that represents city locations and you
+     * start your journey at the first city in the array.
+     *
+     * @param x is an array that represents the cities on a number line. The
+     *          distance between city x[i] and city x[j] is | x[i]-x[j] |
+     *          kilometers.
+     * @param k represents the maximum distance, in kilometers, that one can
+     *          travel in one monorail trip. k should be greater than 0.
+     * @return the number of new cities that one can visit starting from x[0].
+     * x[0] is not included in the count.
+     */
+    public static int howManyCitiesCanIVisit(int[] x, int k) {
+        // todo: Implement this method
+        Set<Integer> visitedSet = new HashSet<Integer>();
+        visitedSet.add(x[0]);
+        int currentCity = x[0];
+        for (int i = 1; i < x.length; i++) {
+            if (valid(x[i], k, visitedSet) && !visitedSet.contains(x[i])) {
+                currentCity = x[i];
+                visitedSet.add(x[i]);
+                i = 0;
+            }
+        }
+        return visitedSet.size() - 1;
+    }
 
-	private static boolean valid(int i, int k, Set<Integer> visitedSet) {
-		boolean result = false;
-		for (int yCity : visitedSet) {
-			if (Math.abs(i - yCity) <= k) {
-				result = true;
-			}
-		}
+    private static boolean valid(int i, int k, Set<Integer> visitedSet) {
+        boolean result = false;
+        for (int yCity : visitedSet) {
+            if (Math.abs(i - yCity) <= k) {
+                result = true;
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
 
 }
--- Exams/Final/Final Part I/src/main/java/ranges/OverlappingRanges.java
+++ Exams/Final/Final Part I/src/main/java/ranges/OverlappingRanges.java
@@ -4,39 +4,35 @@ import java.util.ArrayList;
 
 public class OverlappingRanges {
 
-	/**
-	 * Returns the smallest integer that is present in the maximum number of
-	 * input ranges
-	 * todo smallest is 3
-	 *
-	 * @param startPoints
-	 *            the list of starting points for the ranges
-	 * @param endPoints
-	 *            the list of end points for the ranges
-	 * @return the smallest integer that is present in the maximum number of
-	 *         ranges
-	 * @throws NoOverlapException
-	 *             when there is no overlap between any of the ranges
-	 */
+    /**
+     * Returns the smallest integer that is present in the maximum number of
+     * input ranges
+     * todo smallest is 3
+     *
+     * @param startPoints the list of starting points for the ranges
+     * @param endPoints   the list of end points for the ranges
+     * @return the smallest integer that is present in the maximum number of
+     * ranges
+     * @throws NoOverlapException when there is no overlap between any of the ranges
+     */
 
 
-	public static Integer maxOverlapInt(ArrayList<Integer> startPoints,
-			ArrayList<Integer> endPoints) throws ranges.NoOverlapException {
-		// TODO: Implement this method
-		endPoints.add(n[3]);
+    public static Integer maxOverlapInt(ArrayList<Integer> startPoints,
+                                        ArrayList<Integer> endPoints) throws ranges.NoOverlapException {
+        // TODO: Implement this method
+        endPoints.add(n[3]);
 
-		int validRange = n[3];
-		for (int i = 1; i < x.length; i++) {
-			if (valid(x[i], k, visitedSet) && !visitedSet.contains(x[i])) {
-				validRange = x[i];
-				endPoints.add(x[i]);
-			}
-			return endPoints.size() -1;
-		}
+        int validRange = n[3];
+        for (int i = 1; i < x.length; i++) {
+            if (valid(x[i], k, visitedSet) && !visitedSet.contains(x[i])) {
+                validRange = x[i];
+                endPoints.add(x[i]);
+            }
+            return endPoints.size() - 1;
+        }
 
 
-
-		return 0;
-	}
+        return 0;
+    }
 
 }
