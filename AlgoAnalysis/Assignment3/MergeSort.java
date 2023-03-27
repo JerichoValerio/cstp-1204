60ed580c7350e2f10d693743ce1d8c9ccccf0eb1
JerichoValerio <jerichotvalerio@gmail.com>
2023-03-27 10:23:49 -0700

    formatted code

--- AlgoAnalysis/Assignment3/MergeSort.java
+++ AlgoAnalysis/Assignment3/MergeSort.java
@@ -4,65 +4,64 @@ public class MergeSort {
 
   public static void merge(int[] arr, int l, int m, int r) {
 
-      int n1 = m - l + 1;
-      int n2 = r - m;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-      int[] L = new int[n1];
-      int[] R = new int[n2];
+    int[] L = new int[n1];
+    int[] R = new int[n2];
 
-      for (int i = 0; i < n1; ++i)
-          L[i] = arr[l + i];
-      for (int j = 0; j < n2; ++j)
-          R[j] = arr[m + 1 + j];
+    for (int i = 0; i < n1; ++i)
+      L[i] = arr[l + i];
+    for (int j = 0; j < n2; ++j)
+      R[j] = arr[m + 1 + j];
 
-      int i = 0, j = 0;
+    int i = 0, j = 0;
 
-      int k = l;
-      while (i < n1 && j < n2) {
-          if (L[i] <= R[j]) {
-              arr[k] = L[i];
-              i++;
-          } else {
-              arr[k] = R[j];
-              j++;
-          }
-          k++;
+    int k = l;
+    while (i < n1 && j < n2) {
+      if (L[i] <= R[j]) {
+        arr[k] = L[i];
+        i++;
+      } else {
+        arr[k] = R[j];
+        j++;
       }
+      k++;
+    }
 
-      while (i < n1) {
-          arr[k] = L[i];
-          i++;
-          k++;
-      }
+    while (i < n1) {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
 
-      while (j < n2) {
-          arr[k] = R[j];
-          j++;
-          k++;
-      }
+    while (j < n2) {
+      arr[k] = R[j];
+      j++;
+      k++;
+    }
   }
 
   public static void mergeSort(int[] arr, int l, int r) {
-      if (l < r) {
-          int m = (l + r) / 2;
-          mergeSort(arr, l, m);
-          mergeSort(arr, m + 1, r);
-          merge(arr, l, m, r);
-      }
+    if (l < r) {
+      int m = (l + r) / 2;
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+      merge(arr, l, m, r);
+    }
   }
 
   public static void main(String[] args) {
-      int[] arr = { 12, 11, 13, 5, 6, 7 };
-      System.out.println("Array before sorting:");
-      for (int i = 0; i < arr.length; i++) {
-          System.out.print(arr[i] + " ");
-      }
-      System.out.println();
-      mergeSort(arr, 0, arr.length - 1);
-      System.out.println("Array after sorting:");
-      for (int i = 0; i < arr.length; i++) {
-          System.out.print(arr[i] + " ");
-      }
+    int[] arr = { 12, 11, 13, 5, 6, 7 };
+    System.out.println("Array before sorting:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+    mergeSort(arr, 0, arr.length - 1);
+    System.out.println("Array after sorting:");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
   }
 }
-
