97e2aac02724b72c6ec35ed1d46002f826249852
JerichoValerio <jerichotvalerio@gmail.com>
2023-03-27 10:22:33 -0700

    MergeSort implemented

--- AlgoAnalysis/Assignment3/MergeSort.java
+++ AlgoAnalysis/Assignment3/MergeSort.java
@@ -1,43 +1,67 @@
 package AlgoAnalysis.Assignment3;
 
 public class MergeSort {
-  public void sort(int[] arr) {
-      int n = arr.length;
-      if (n < 2) return;
-      int mid = n / 2;
-      int[] left = new int[mid];
-      int[] right = new int[n - mid];
 
-      // Copy data to temporary arrays
-      for (int i = 0; i < mid; i++) {
-          left[i] = arr[i];
+  public static void merge(int[] arr, int l, int m, int r) {
+
+      int n1 = m - l + 1;
+      int n2 = r - m;
+
+      int[] L = new int[n1];
+      int[] R = new int[n2];
+
+      for (int i = 0; i < n1; ++i)
+          L[i] = arr[l + i];
+      for (int j = 0; j < n2; ++j)
+          R[j] = arr[m + 1 + j];
+
+      int i = 0, j = 0;
+
+      int k = l;
+      while (i < n1 && j < n2) {
+          if (L[i] <= R[j]) {
+              arr[k] = L[i];
+              i++;
+          } else {
+              arr[k] = R[j];
+              j++;
+          }
+          k++;
       }
-      for (int i = mid; i < n; i++) {
-          right[i - mid] = arr[i];
+
+      while (i < n1) {
+          arr[k] = L[i];
+          i++;
+          k++;
       }
 
-      // Recursively sort both halves
-      sort(left);
-      sort(right);
-
-      // Merge the sorted halves
-      merge(arr, left, right);
+      while (j < n2) {
+          arr[k] = R[j];
+          j++;
+          k++;
+      }
   }
 
-  void merge(int[] arr, int[] left, int[] right) {
-      int i = 0, j = 0, k = 0;
-      while (i < left.length && j < right.length) {
-          if (left[i] <= right[j]) {
-              arr[k++] = left[i++];
-          } else {
-              arr[k++] = right[j++];
-          }
+  public static void mergeSort(int[] arr, int l, int r) {
+      if (l < r) {
+          int m = (l + r) / 2;
+          mergeSort(arr, l, m);
+          mergeSort(arr, m + 1, r);
+          merge(arr, l, m, r);
       }
-      while (i < left.length) {
-          arr[k++] = left[i++];
+  }
+
+  public static void main(String[] args) {
+      int[] arr = { 12, 11, 13, 5, 6, 7 };
+      System.out.println("Array before sorting:");
+      for (int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
       }
-      while (j < right.length) {
-          arr[k++] = right[j++];
+      System.out.println();
+      mergeSort(arr, 0, arr.length - 1);
+      System.out.println("Array after sorting:");
+      for (int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
       }
   }
 }
